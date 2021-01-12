3608cf899160e20135ccf89f3b747947f7c1ad89
ambrt <ambrth@gmail.com>
2021-01-12 08:29:25 +0100

    waiting for 1.6.8 or higher release

--- src/index.ts
+++ src/index.ts
@@ -27,48 +27,49 @@ joplin.plugins.register({
 			public: true,
 			label: 'Place backlinks on the bottom (checked) or at the top (unchecked) ',
 		});
-		
+
 		await joplin.commands.register({
 			name: "insertBackReferences",
 			label: "Insert references",
-			iconName:"fas fa-hand-point-left",
+			iconName: "fas fa-hand-point-left",
 			execute: async () => {
 
 				let data = await joplin.workspace.selectedNote();
 				let body = data.body;
 				let notes
-				let has_more =true
+				let has_more = true
 				let page = 1
 				let references = await joplin.settings.value('myBacklinksCustomSetting');
 				while (has_more) {
-					notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page:page });
-				
+					notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
+
 					for (let i = 0; i < notes.items.length; i++) {
 						let element = notes.items[i];
 						references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
-					}	
+					}
 					if (notes.has_more) { page = page + 1 } else { has_more = false }
-	
+
 				}
 
 				let newData
 				let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
-				if(topOrBottom){
-					newData = body+references.replace(/\\n/g,"\n")
-				}else{
-					let bodyArr=body.split("\n")
-					bodyArr.splice(1, 0, references.replace(/\\n/g,"\n"));
+				if (topOrBottom) {
+					newData = body + references.replace(/\\n/g, "\n")
+				} else {
+					let bodyArr = body.split("\n")
+					bodyArr.splice(1, 0, references.replace(/\\n/g, "\n"));
 					newData = bodyArr.join("\n")
 				}
-				
-				
+
+
 				await joplin.commands.execute('textSelectAll')
 				await joplin.commands.execute('replaceSelection', newData)
 
 			}
 		});
 		await joplin.views.toolbarButtons.create('insertRefsToolbar', 'insertBackReferences', ToolbarButtonLocation.EditorToolbar);
-		await joplin.views.menuItems.create('inserRefsMenu', 'insertBackReferences', MenuItemLocation.Note, {accelerator:"Ctrl+Alt+B"});
+		await joplin.views.menuItems.create('inserRefsMenu', 'insertBackReferences', MenuItemLocation.Note, { accelerator: "Ctrl+Alt+B" });
+		/*
 		await joplin.commands.register({
 			name: 'getCont',
 			label: 'returns backlinks html',
@@ -77,7 +78,77 @@ joplin.plugins.register({
 				return "<b> testing html from command</b>"
 			},
 		});
+		*/
 		console.info('Test plugin started!');
+		/*
+		let contentScriptId = "backlinks"
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			contentScriptId,
+			'./contentScriptMarkdownIt.js'
+		);
+
+		await joplin.contentScripts.onMessage(contentScriptId, async (message: any) => {
+			//get content
+			if (message.type == "getContent") {
+				let data = await joplin.workspace.selectedNote();
+				let body = data.body;
+				let notes
+				let has_more = true
+				let page = 1
+				let references = await joplin.settings.value('myBacklinksCustomSetting');
+				let notesHtml
+				while (has_more) {
+					notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
+
+					for (let i = 0; i < notes.items.length; i++) {
+						let element = notes.items[i];
+						//references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
+						references = references + "<br>" + `<a href="#" onclick="webviewApi.postMessage('${contentScriptId}', {type:'openNote',noteId:'${element.id}'})">${escapeTitleText(element.title)}</a><br>`;
+					if (notes.has_more) { page = page + 1 } else { has_more = false }
+
+				}
+			}
+				let newData
+				let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
+				if (topOrBottom) {
+					newData = body + references.replace(/\\n/g, "<br>")
+				} else {
+					let bodyArr = body.split("<br>")
+					
+					//inserting references here
+					bodyArr.splice(1, 0, references.replace(/\\n/g, "<br>"));
+					
+					newData = bodyArr.join("<br>")
+				}
+
+			
+				let response = `<b>Backlinks!!</b><br>${newData}`
+				return response;
+			
+			}
+			else if (message.type = "openNote") {
+				await joplin.commands.execute('openNote', message.noteId);
+			}
+			else {
+
+				console.log("some error occured")
+				console.log(message)
+				console.info("some error occured")
+				console.info(message)
+			}
+			
+
+			return false;
+		});
+
+
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			contentScriptId,
+			'./notesReferences.js'
+		);
+*/
 		/*
 		await joplin.plugins.registerContentScript(
 			ContentScriptType.MarkdownItPlugin,
--- src/notesReferences.js
+++ src/notesReferences.js
@@ -1,11 +1,25 @@
 
 function plugin(md, _options) {
     console.log(_options)
-    
+    const contentScriptId = context.contentScriptId;
 
     function render_footnote_block_open2(tokens, idx, options) {
-
-        return `<div id="backref">back</div><a href="#" onclick="let cont='asdf';async function a(){cont=await webviewApi.executeCommand('getFun');webviewApi.executeCommand('getFun').then(res=>console.log(res));document.getElementById('backref').innerHTML=cont};a();">asdf</a>`
+        
+        const postMessageWithResponseTest = `
+        webviewApi.postMessage('${contentScriptId}', {type:'getContent'}).then(function(response) {
+            console.info('Got response in Markdown-it content script: ' + response);
+            document.getElementById('backs').innerHTML=response
+        });
+        return false;
+    `;
+        //return `<div id="backref">back</div><a href="#" onclick="let cont='asdf';async function a(){cont=await webviewApi.executeCommand('getFun');webviewApi.executeCommand('getFun').then(res=>console.log(res));document.getElementById('backref').innerHTML=cont};a();">asdf</a>`
+      return `
+        <div style="padding:10px; border: 1px solid green;">
+            <p><a href="#" onclick="${postMessageWithResponseTest.replace(/\n/g, ' ')}">Click to post a message to plugin and check the response in the console</a></p>
+        </div>
+        <div id="backs"></div>
+        <style onload="${postMessageWithResponseTest.replace(/\n/g, ' ')}"/>
+    `;
     //return `<div id="backref">back</div><a href="#" onclick="document.getElementById('backref').innerHTML='myasdf';">asdf</a>`
         
       }

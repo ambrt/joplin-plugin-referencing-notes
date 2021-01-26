4f4bf67c5ca3b3f698c2f5c824c1c5047c76eeb7
ambrt <ambrth@gmail.com>
2021-01-26 13:37:07 +0100

    working 2.0.0

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,13 @@ joplin.plugins.register({
 			iconName: 'fas fa-hand-point-left',
 		});
 		await joplin.settings.registerSetting('myBacklinksCustomSettingHeader', {
-			value: "\\n\\n## References\\n",
+			value: "\\n\\n## Backlinks\\n",
 			type: 2,
 			section: 'myBacklinksCustomSection',
 			public: true,
 			label: 'Heading above list of backlinks (use "\\n" as a new line)',
 		});
-		/*
+
 		await joplin.settings.registerSetting('myBacklinksCustomSettingAutoMarkdown', {
 			value: true,
 			type: 3,
@@ -28,7 +28,7 @@ joplin.plugins.register({
 			public: true,
 			label: 'Automatic backlinks section on every note',
 		});
-*/
+
 
 
 
@@ -37,7 +37,7 @@ joplin.plugins.register({
 			type: 3,
 			section: 'myBacklinksCustomSection',
 			public: true,
-			label: 'Place backlinks on the bottom (checked) or at the top (unchecked) ',
+			label: 'Place "manual"  backlinks on the bottom (checked) or at the top (unchecked) ',
 		});
 
 		await joplin.commands.register({
@@ -81,87 +81,96 @@ joplin.plugins.register({
 		});
 		await joplin.views.toolbarButtons.create('insertRefsToolbar', 'insertBackReferences', ToolbarButtonLocation.EditorToolbar);
 		await joplin.views.menuItems.create('inserRefsMenu', 'insertBackReferences', MenuItemLocation.Note, { accelerator: "Ctrl+Alt+B" });
-		/*
-		await joplin.commands.register({
-			name: 'getCont',
-			label: 'returns backlinks html',
-			execute: async () => {
-				console.log("got called getFun()")
-				return "<b> testing html from command</b>"
-			},
-		});
-		*/
-		console.info('Test plugin started!');
-		/*
 		let contentScriptId = "backlinks"
+
+		await joplin.contentScripts.onMessage(contentScriptId, async (message:any) => {
+			console.info('PostMessagePlugin (CodeMirror ContentScript): Got message:', message);
+			//const response = message + '+responseFromCodeMirrorScriptHandler';
+			//console.info('PostMessagePlugin (CodeMirror ContentScript): Responding with:', response);
+			//return response;
+			let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
+				console.log("got called getFun()")
+				//return "<b> testing html from command</b>"
+				//get content
+				let isAuto=await joplin.settings.value('myBacklinksCustomSettingAutoMarkdown')
+				if (message.type == "getContent" && isAuto) {
+					console.log("getting content")
+					let data = await joplin.workspace.selectedNote();
+					let body = data.body;
+					let notes
+					let has_more = true
+					let page = 1
+					let referenceHeader = await joplin.settings.value('myBacklinksCustomSettingHeader');
+					let notesHtml
+					let references =""
+					while (has_more) {
+						notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
+						console.log(notes)
+						
+						for (let i = 0; i < notes.items.length; i++) {
+							let element = notes.items[i];
+							//references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
+							references = references + "" + `<a href="#" onclick="webviewApi.postMessage('${contentScriptId}', {type:'openNote',noteId:'${element.id}'})">${escapeTitleText(element.title)}</a><br>`;
+							if (notes.has_more) { page = page + 1 } else { has_more = false }
+
+						}
+					}
+					let newData = references
+					let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
+					if (topOrBottom) {
+						//newData = references.replace(/\\n/g, "<br>")
+					} else {
+						//let bodyArr = body.split("<br>")
+
+						//inserting references here
+						//bodyArr.splice(1, 0, references.replace(/\\n/g, "<br>"));
+
+						//newData = bodyArr.join("<br>")
+					}
+
+
+					let response = `<h3>${referenceHeader.replace(/\\n/g,"<br>")}</h3>${newData}`
+					return response;
+
+
+				} else if (message.type =="openNote") {
+					await joplin.commands.execute('openNote', message.noteId);
+					console.log("open note")
+				}
+				else {
+
+					console.log("some error occured");
+					console.log(message);
+					console.info("some error occured");
+					console.info(message);
+					return false
+				}
+
+
+			
+
+			});
+
+
+		console.info('Test plugin started!');
+
+		
 		await joplin.contentScripts.register(
 			ContentScriptType.MarkdownItPlugin,
 			contentScriptId,
 			'./notesReferences.js'
 		);
 
-			
-			//get content
-			
-			if (message.type == "getContent" && await joplin.settings.value('myBacklinksCustomSettingAutoMarkdown'); ) {
-				let data = await joplin.workspace.selectedNote();
-				let body = data.body;
-				let notes
-				let has_more = true
-				let page = 1
-				let references = await joplin.settings.value('myBacklinksCustomSetting');
-				let notesHtml
-				while (has_more) {
-					notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
-
-					for (let i = 0; i < notes.items.length; i++) {
-						let element = notes.items[i];
-						//references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
-						references = references + "<br>" + `<a href="#" onclick="webviewApi.postMessage('${contentScriptId}', {type:'openNote',noteId:'${element.id}'})">${escapeTitleText(element.title)}</a><br>`;
-					if (notes.has_more) { page = page + 1 } else { has_more = false }
-
-				}
-			}
-				let newData
-				let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
-				if (topOrBottom) {
-					newData = body + references.replace(/\\n/g, "<br>")
-				} else {
-					let bodyArr = body.split("<br>")
-					
-					//inserting references here
-					bodyArr.splice(1, 0, references.replace(/\\n/g, "<br>"));
-					
-					newData = bodyArr.join("<br>")
-				}
-
-			
-				let response = `<b>Backlinks!!</b><br>${newData}`
-				return response;
-			
-			}
-			else if (message.type = "openNote") {
-				await joplin.commands.execute('openNote', message.noteId);
-			}
-			else {
-
-				console.log("some error occured")
-				console.log(message)
-				console.info("some error occured")
-				console.info(message)
-			}
-			
-
-			return false;
-		});
 
 
-		await joplin.contentScripts.register(
-			ContentScriptType.MarkdownItPlugin,
-			contentScriptId,
-			'./notesReferences.js'
-		);
-*/
+
+		/*
+				await joplin.contentScripts.register(
+					ContentScriptType.MarkdownItPlugin,
+					contentScriptId,
+					'./notesReferences.js'
+				);
+		*/
 		/*
 		await joplin.plugins.registerContentScript(
 			ContentScriptType.MarkdownItPlugin,
--- src/notesReferences.js
+++ src/notesReferences.js
@@ -1,71 +1,73 @@
 
-function plugin(md, _options) {
-    console.log(_options)
-    const contentScriptId = context.contentScriptId;
-
-    function render_footnote_block_open2(tokens, idx, options) {
-        
-        const postMessageWithResponseTest = `
-        webviewApi.postMessage('${contentScriptId}', {type:'getContent'}).then(function(response) {
-            console.info('Got response in Markdown-it content script: ' + response);
-            document.getElementById('backs').innerHTML=response;
-        });
-        return false;
-    `;
-        //return `<div id="backref">back</div><a href="#" onclick="let cont='asdf';async function a(){cont=await webviewApi.executeCommand('getFun');webviewApi.executeCommand('getFun').then(res=>console.log(res));document.getElementById('backref').innerHTML=cont};a();">asdf</a>`
-      return `
-        <div style="padding:10px; border: 1px solid green;">
-            <p>Test<a href="#" onclick="${postMessageWithResponseTest.replace(/\n/g, ' ')}">Click to post a message to plugin and check the response in the console</a></p>
-        </div>
-        <div id="backs"></div>
-        <style onload="${postMessageWithResponseTest.replace(/\n/g, ' ')}"/>
-    `;
-    //return `<div id="backref">back</div><a href="#" onclick="document.getElementById('backref').innerHTML='myasdf';">asdf</a>`
-        
-      }
-      
-
-    md.renderer.rules.footnote_block_open2   = render_footnote_block_open2;
-    
-
-    function footnote_tail2(state) {
- 
-
-        token = new state.Token('footnote_block_open2', '', 1);
-        state.tokens.push(token);
-  }
-  md.core.ruler.after('inline', 'footnote_tail2', footnote_tail2);
-
-    /*
-    var defaultRender = markdownIt.renderer.rules.text;
-    markdownIt.renderer.rules.text =
-    function (tokens, idx, options, env, self) {
-        var token = tokens[idx];
-        console.log(token);
-        console.log(idx)
-        console.log("-------");
-        console.log(tokens.length);
-        if(idx==1){
-            
-            //token.attrs.push(['js', "webviewApi.executeCommand('testCommand', 'one', 'two'); return false;"]);
-            let replacement={html:"<h1>ADFS</h1>"}
-    
-            return replacement.html;
-            
-            
-        }
-
-
-        return defaultRender(tokens, idx, options, env, self);
-        
-    }
-    */
-}
 
 module.exports = {
 	default: function(_context) { 
 		return {
-			plugin: plugin,
+			plugin: function (md, _options) {
+                console.log(_options)
+                const contentScriptId = _context.contentScriptId;
+            
+                function render_footnote_block_open2(tokens, idx, options) {
+                    
+                    const postMessageWithResponseTest = `
+                    webviewApi.postMessage('${contentScriptId}', {type:'getContent'}).then(function(response) {
+                        console.info('Got response in Markdown-it content script: ' + response);
+                        document.getElementById('backs').innerHTML=response;
+                    });
+                    return false;
+                `;
+                    //return `<div id="backref">back</div><a href="#" onclick="let cont='asdf';async function a(){cont=await webviewApi.executeCommand('getFun');webviewApi.executeCommand('getFun').then(res=>console.log(res));document.getElementById('backref').innerHTML=cont};a();">asdf</a>`
+                  return `
+                    <div id="backs"></div>
+                    <style onload="${postMessageWithResponseTest.replace(/\n/g, ' ')}"/>
+                `;
+                //return `<div id="backref">back</div><a href="#" onclick="document.getElementById('backref').innerHTML='myasdf';">asdf</a>`
+                    
+                  }
+                  
+            
+                md.renderer.rules.footnote_block_open2   = render_footnote_block_open2;
+                
+            
+                function footnote_tail2(state) {
+             
+            
+                    let token = new state.Token('footnote_block_open2', '', 0);
+                    
+                    state.tokens.push(token);
+                    console.log("foot tokens");
+                    console.log(state.tokens)
+              }
+              md.core.ruler.after('inline', 'footnote_tail2', footnote_tail2);
+            
+                /*
+                var defaultRender = md.renderer.rules.text;
+                md.renderer.rules.text =
+                function (tokens, idx, options, env, self) {
+                    var token = tokens[idx];
+                    console.log(token);
+                    console.log(idx)
+                    console.log("-------");
+                    console.log(tokens.length);
+                    if(idx==0){
+                        
+                        //token.attrs.push(['js', "webviewApi.executeCommand('testCommand', 'one', 'two'); return false;"]);
+                        let first = md.utils.escapeHtml(token.content)
+                        let replacement={html:`${first}<h1>ADFS</h1>`}
+                
+                        return replacement.html;
+                        
+                        
+                    }
+            
+            
+                    return defaultRender(tokens, idx, options, env, self);
+                    
+                }
+                */
+                
+            }
+            
 			
 		}
 	},

ec003c9ff3810c5a7b015727ce78808e800c17b2
ambrt <ambrth@gmail.com>
2021-01-27 19:22:34 +0100

    fixes #2

--- src/index.ts
+++ src/index.ts
@@ -40,9 +40,24 @@ joplin.plugins.register({
 			label: 'Place "manual"  backlinks on the bottom (checked) or at the top (unchecked) ',
 		});
 
+		await joplin.settings.registerSetting('myBacklinksCustomHideBacklinksIfNoneAuto', {
+			value: false,
+			type: 3,
+			section: 'myBacklinksCustomSection',
+			public: true,
+			label: "Hide auto-backlinks section if there are no backlinks"
+		});
+		await joplin.settings.registerSetting('myBacklinksCustomSettingUseHint', {
+			value: true,
+			type: 3,
+			section: 'myBacklinksCustomSection',
+			public: true,
+			label: "Use text hint if there are no backlinks (manual insert)"
+		});
+
 		await joplin.commands.register({
 			name: "insertBackReferences",
-			label: "Insert references",
+			label: "Insert backlinks",
 			iconName: "fas fa-hand-point-left",
 			execute: async () => {
 
@@ -52,12 +67,15 @@ joplin.plugins.register({
 				let has_more = true
 				let page = 1
 				let references = await joplin.settings.value('myBacklinksCustomSettingHeader');
+				let thereAreNotes = false
+				let useManualHint = await joplin.settings.value('myBacklinksCustomSettingUseHint')
 				while (has_more) {
 					notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
 
 					for (let i = 0; i < notes.items.length; i++) {
 						let element = notes.items[i];
 						references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
+						thereAreNotes=true
 					}
 					if (notes.has_more) { page = page + 1 } else { has_more = false }
 
@@ -65,6 +83,9 @@ joplin.plugins.register({
 
 				let newData
 				let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
+				if(!thereAreNotes && useManualHint){
+					references = references + "\n<small><font color='grey'><i>No backlinks</i></font></small>"
+				}
 				if (topOrBottom) {
 					newData = body + references.replace(/\\n/g, "\n")
 				} else {
@@ -83,79 +104,88 @@ joplin.plugins.register({
 		await joplin.views.menuItems.create('inserRefsMenu', 'insertBackReferences', MenuItemLocation.Note, { accelerator: "Ctrl+Alt+B" });
 		let contentScriptId = "backlinks"
 
-		await joplin.contentScripts.onMessage(contentScriptId, async (message:any) => {
+		await joplin.contentScripts.onMessage(contentScriptId, async (message: any) => {
 			console.info('PostMessagePlugin (CodeMirror ContentScript): Got message:', message);
-			//const response = message + '+responseFromCodeMirrorScriptHandler';
-			//console.info('PostMessagePlugin (CodeMirror ContentScript): Responding with:', response);
-			//return response;
+
 			let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
-				console.log("got called getFun()")
-				//return "<b> testing html from command</b>"
-				//get content
-				let isAuto=await joplin.settings.value('myBacklinksCustomSettingAutoMarkdown')
-				if (message.type == "getContent" && isAuto) {
-					console.log("getting content")
-					let data = await joplin.workspace.selectedNote();
-					let body = data.body;
-					let notes
-					let has_more = true
-					let page = 1
-					let referenceHeader = await joplin.settings.value('myBacklinksCustomSettingHeader');
-					let notesHtml
-					let references =""
-					while (has_more) {
-						notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
-						console.log(notes)
-						
-						for (let i = 0; i < notes.items.length; i++) {
-							let element = notes.items[i];
-							//references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
-							references = references + "" + `<a href="#" onclick="webviewApi.postMessage('${contentScriptId}', {type:'openNote',noteId:'${element.id}'})">${escapeTitleText(element.title)}</a><br>`;
-							
+			console.log("got called getFun()")
+			//get content
+			let isAuto = await joplin.settings.value('myBacklinksCustomSettingAutoMarkdown')
+			if (message.type == "getContent" && isAuto) {
+				console.log("getting content")
+				let data = await joplin.workspace.selectedNote();
+				let body = data.body;
+				let notes
+				let has_more = true
+				let page = 1
+				let referenceHeader = await joplin.settings.value('myBacklinksCustomSettingHeader');
+				let hideIfNoBacklinks = await joplin.settings.value('myBacklinksCustomHideBacklinksIfNoneAuto');
+				let notesHtml
+				let references = ""
+				let thereAreAutoBacklinks = false
+				while (has_more) {
+					notes = await joplin.data.get(['search'], { query: data.id, fields: ['id', 'title', 'body'], page: page });
+					console.log(notes)
 
-						}
-						if (notes.has_more) { page = page + 1 } else { has_more = false }
+					for (let i = 0; i < notes.items.length; i++) {
+						let element = notes.items[i];
+						//references = references + "\n" + `[${escapeTitleText(element.title)}](:/${element.id})`;
+						references = references + "" + `<a href="#" onclick="webviewApi.postMessage('${contentScriptId}', {type:'openNote',noteId:'${element.id}'})">${escapeTitleText(element.title)}</a><br>`;
+						thereAreAutoBacklinks = true
+
+
 					}
-					let newData = references
-					let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
-					if (topOrBottom) {
-						//newData = references.replace(/\\n/g, "<br>")
-					} else {
-						//let bodyArr = body.split("<br>")
-
-						//inserting references here
-						//bodyArr.splice(1, 0, references.replace(/\\n/g, "<br>"));
-
-						//newData = bodyArr.join("<br>")
-					}
-
-
-					let response = `<h3>${referenceHeader.replace(/\\n/g,"<br>")}</h3>${newData}`
-					return response;
-
-
-				} else if (message.type =="openNote") {
-					await joplin.commands.execute('openNote', message.noteId);
-					console.log("open note")
+					if (notes.has_more) { page = page + 1 } else { has_more = false }
 				}
-				else {
 
-					console.log("some error occured");
-					console.log(message);
-					console.info("some error occured");
-					console.info(message);
-					return ''
+				if(!thereAreAutoBacklinks){
+					references = references + "\n<small><font color='grey'><i>No backlinks</i></font></small>"
+				}
+				let newData = references
+				let topOrBottom = await joplin.settings.value('myBacklinksCustomSettingTopOrBottom');
+				if (topOrBottom) {
+					//newData = references.replace(/\\n/g, "<br>")
+				} else {
+					//let bodyArr = body.split("<br>")
+
+					//inserting references here
+					//bodyArr.splice(1, 0, references.replace(/\\n/g, "<br>"));
+
+					//newData = bodyArr.join("<br>")
 				}
 
 
-			
+				let response=''
+				if(!hideIfNoBacklinks){
+					response = `<h3>${referenceHeader.replace(/\\n/g, "<br>")}</h3>${newData}`
+				}
+				
 
-			});
+				return response;
+
+
+			} else if (message.type == "openNote") {
+				await joplin.commands.execute('openNote', message.noteId);
+				console.log("open note")
+			}
+			else {
+
+				console.log("some error occured");
+				console.log(message);
+				console.info("some error occured");
+				console.info(message);
+				return ''
+			}
+
+
+
+
+		});
 
 
 		console.info('Test plugin started!');
 
-		
+
 		await joplin.contentScripts.register(
 			ContentScriptType.MarkdownItPlugin,
 			contentScriptId,
